68c8c85479ecd400db7ee47621cdb6892f4d0afc
Wilson Hsu <40781627+wilsonhsu0102@users.noreply.github.com>
2018-11-29 23:32:09 -0500

    Modified slidingTileBoardMAnagerTest so code coverage is 100%, Added JovaDoc to SavedGamesManagerTest.

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/gamecentre/SavedGamesManagerTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/gamecentre/SavedGamesManagerTest.java
@@ -10,62 +10,108 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import fall2018.csc2017.cardmatching.Card;
-import fall2018.csc2017.cardmatching.CardMatchingBoard;
 import fall2018.csc2017.cardmatching.CardMatchingBoardManager;
 
 import static org.junit.Assert.*;
 
-
 /**
- * Test SavedGamesManager
+ * Test SavedGamesManager.
  */
 public class SavedGamesManagerTest {
 
+    /**
+     * A SavedGamesManager.
+     */
     private SavedGamesManager savedGamesManager = new SavedGamesManager();
 
-    private CardMatchingBoardManager cardMatchingBoardManager = new CardMatchingBoardManager(8, true);
+    /**
+     * A CardMatchingBoardManager.
+     */
+    private CardMatchingBoardManager cardMatchingBoardManager;
 
+    /**
+     * A GameToSave.
+     */
     private GameToSave gameToSave;
 
+    /**
+     * All saved games for all users of all game type.
+     */
     private Map<String, Map<String, ArrayList<GameToSave>>> savedGames = new HashMap<>();
 
+    /**
+     * All saved games of all game type for specific user.
+     */
     private Map<String, ArrayList<GameToSave>> savedGamesByUser = new HashMap<>();
 
+    /**
+     * All saved games of a game type for specific user.
+     */
     private ArrayList<GameToSave> savedGamesByGame = new ArrayList<>();
 
+    /**
+     * A User
+     */
     private User user;
 
+    /**
+     * Name of a game.
+     */
     private String nameOfGame;
 
+    /**
+     * Set Up all variables for testing.
+     */
     @Before
     public void setUp() {
+        cardMatchingBoardManager = new CardMatchingBoardManager(8, true);
+        setUpSavedGamesByGame();
+        savedGamesManager = new SavedGamesManager(savedGames);
+        user = new User("User1", "1");
+        nameOfGame =  getTime() + " (" + cardMatchingBoardManager.getGameDifficulty() + ") ";
+    }
+
+    /**
+     * Set up savedGamesByGame.
+     */
+    private void setUpSavedGamesByGame() {
         CardMatchingBoardManager cardMatchingBoardManager1 = new CardMatchingBoardManager(10, true);
         GameToSave gameToSave1 = new GameToSave(cardMatchingBoardManager1);
         CardMatchingBoardManager cardMatchingBoardManager2 = new CardMatchingBoardManager(10, true);
         GameToSave gameToSave2 = new GameToSave(cardMatchingBoardManager2);
-
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
-
         gameToSave = new GameToSave(cardMatchingBoardManager);
-        savedGamesManager = new SavedGamesManager(savedGames);
         savedGamesByGame.add(gameToSave);
         savedGamesByGame.add(gameToSave1);
         savedGamesByGame.add(gameToSave2);
-        user = new User("User1", "1");
-        nameOfGame = dateTimeFormatter.format(now) + " (" + cardMatchingBoardManager.getGameDifficulty() + ") ";
     }
 
+    /**
+     * Get time of the game saved.
+     */
+    private String getTime() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        return dateTimeFormatter.format(now);
+    }
+
+    /**
+     * Makes all variable equals to null.
+     */
     @After
     public void tearDown(){
+        cardMatchingBoardManager = null;
         gameToSave = null;
         savedGamesManager = null;
+        savedGamesByGame = null;
+        nameOfGame = null;
         user = null;
     }
 
+    /**
+     * Test whether updateSavedGames works with empty savedGames.
+     */
     @Test
-    public void testUpdateSavedGamesWithEmptyStart(){
+    public void testUpdateSavedGamesWithEmptyMap(){
         assertEquals(0, savedGames.size());
         savedGamesManager.updateSavedGames(gameToSave, "User1");
         assertEquals(1, savedGames.size());
@@ -73,6 +119,9 @@ public class SavedGamesManagerTest {
         assertEquals(1, savedGames.get("User1").get("Card Matching").size());
     }
 
+    /**
+     * Test whether updateSavedGames works with same user but different game type.
+     */
     @Test
     public void testUpdateSavedGamesWithSameUserDifferentGameType(){
         savedGamesByUser.put("Sliding Tile", savedGamesByGame);
@@ -84,6 +133,9 @@ public class SavedGamesManagerTest {
         assertEquals(1, savedGames.get("User1").get("Card Matching").size());
     }
 
+    /**
+     * Test whether updateSavedGames works with same user, same game type, different board.
+     */
     @Test
     public void testUpdateSavedGamesWithSameUserSameGameTypeDifferentBoard(){
         CardMatchingBoardManager newCardMatchingBoard = new CardMatchingBoardManager(12, true);
@@ -96,6 +148,9 @@ public class SavedGamesManagerTest {
         assertEquals(2, savedGames.get("User1").get("Card Matching").size());
     }
 
+    /**
+     * Test whether updateSavedGames works with same user, same game type, same board.
+     */
     @Test
     public void testUpdateSavedGamesWithSameUserSameGameTypeSameBoard(){
         CardMatchingBoardManager newCardMatchingBoard = cardMatchingBoardManager;
@@ -108,6 +163,9 @@ public class SavedGamesManagerTest {
         assertEquals(1, savedGames.get("User1").get("Card Matching").size());
     }
 
+    /**
+     * Test whether updateSavedGames works with different user.
+     */
     @Test
     public void testUpdateSavedGamesWithDifferentUser(){
         assertEquals(0, savedGames.size());
@@ -120,8 +178,11 @@ public class SavedGamesManagerTest {
         assertEquals(2, savedGames.size());
     }
 
+    /**
+     * Test whether updateSavedGames removes game with same board.
+     */
     @Test
-    public void testUpdateSavedGameRemoveSameGame(){
+    public void testUpdateSavedGameRemoveSameBoard(){
         savedGamesByUser.put("Card Matching", savedGamesByGame);
         savedGames.put("User1", savedGamesByUser);
         assertEquals(3, savedGames.get("User1").get("Card Matching").size());
@@ -129,6 +190,9 @@ public class SavedGamesManagerTest {
         assertEquals(3, savedGames.get("User1").get("Card Matching").size());
     }
 
+    /**
+     * Test whether updateSavedGames removes game when number of games saved goes over 3.
+     */
     @Test
     public void testUpdateSavedGameRemoveExtraGame(){
         savedGamesByUser.put("Card Matching", savedGamesByGame);
@@ -140,15 +204,23 @@ public class SavedGamesManagerTest {
         assertEquals(3, savedGames.get("User1").get("Card Matching").size());
     }
 
+    /**
+     * Test whether constructNameArray works on empty savedGames.
+     */
     @Test
     public void testConstructNameArrayOnEmptyMap(){
         ArrayList<String> nameArray = new ArrayList<>();
         savedGames.put("User1", savedGamesByUser);
         assertEquals(0, nameArray.size());
         nameArray = savedGamesManager.constructNameArray("Card Matching", user);
-        assertEquals(1, nameArray.size());
+        ArrayList<String> expectedArray = new ArrayList<>();
+        expectedArray.add("Select a Saved Game");
+        assertEquals(expectedArray, nameArray);
     }
 
+    /**
+     * Test whether constructNameArray works.
+     */
     @Test
     public void testConstructNameArray(){
         ArrayList<String> nameArray = new ArrayList<>();
@@ -164,12 +236,18 @@ public class SavedGamesManagerTest {
         assertEquals(expectedArray, nameArray);
     }
 
+    /**
+     * Test whether getSelectedGameToSave return the right GameManager.
+     */
     @Test
     public void testGetSelectedGameToSave(){
         GameManager result = savedGamesManager.getSelectedGameToSave(nameOfGame, savedGamesByGame);
         assertEquals(cardMatchingBoardManager, result);
     }
 
+    /**
+     * Test whether getSavedGames works.
+     */
     @Test
     public void testGetSavedGames(){
         assertEquals(savedGames, savedGamesManager.getSavedGames());
